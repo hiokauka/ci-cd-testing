290cd177c075c2109c5791567657ea5b5c0cc4be
Rauf Hazim <raufhazim07@gmail.com>
2025-07-30 10:21:53 +0800

    testing2

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should return greeting text', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hello from Node.js app with CI/CD!');
+  });
+});
